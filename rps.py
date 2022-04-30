b95c940a7fce2106446db5ab92f4b4612358a8e6
Matthew McCane <MatthewLeeMcCane@gmail.com>
2022-04-30 19:30:08 -0400

    added class for human player and tracked score

--- rps.py
+++ rps.py
@@ -11,6 +11,7 @@ in this game"""
 
 
 class Player:
+
     def move(self):
         return 'rock'
 
@@ -18,18 +19,48 @@ class Player:
         pass
 
 
+"""This class is for a human player."""
+
+
+class HumanPlayer:
+    def move(self):
+        response = input("Please select: Rock, Paper, or Scissors\n").lower()
+        if "rock" in response:
+            return "You have selected rock."
+        elif "paper" in response:
+            return "You have selected paper."
+        elif "scissors" in response:
+            return "You have selected scissors."
+        else:
+            self.move()
+
+    def learn(self, my_move, their_move):
+        pass
+
+
+"""This class is for a Random Player"""
+
+
 class RandomPlayer:
     def move(self):
         return random.choice(moves)
 
+    def learn(self, my_move, their_move):
+        pass
+
 
 def beats(one, two):
+
     return ((one == 'rock' and two == 'scissors') or
             (one == 'scissors' and two == 'paper') or
             (one == 'paper' and two == 'rock'))
 
 
 class Game:
+    """These class level variables keep count of score """
+    p1_score = 0
+    p2_score = 0
+
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
@@ -41,6 +72,27 @@ class Game:
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
 
+    # This logic checks which player won and displays the new score.
+
+        if move1 == move2:
+            print("This round was a tie.\n"
+                  f"Player 1 Score: {self.p1_score}\n"
+                  f"Player 2 score: {self.p2_score}")
+
+        elif beats(move1, move2):
+
+            self.p1_score += 1
+            print("Player 1 wins!\n"
+                  f"Player 1 Score: {self.p1_score}\n"
+                  f"Player 2 score: {self.p2_score}")
+
+        else:
+
+            self.p2_score += 1
+            print("Player 2 wins!\n"
+                  f"Player 1 Score: {self.p1_score}\n"
+                  f"Player 2 score: {self.p2_score}")
+
     def play_game(self):
         print("Welcome to the Rock Paper Scissors game!\n")
         for round in range(3):
@@ -50,5 +102,5 @@ class Game:
 
 
 if __name__ == '__main__':
-    game = Game(Player(), Player())
+    game = Game(RandomPlayer(), HumanPlayer())
     game.play_game()
